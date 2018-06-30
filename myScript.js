13017c2c34d732cdbc8bf1b1ffa45911a23344fc
whyad <halfmoonmonk@gmail.com>
2018-06-30 19:47:46 +0400

    it flashes

---
+++ myScript.js
@@ -0,0 +1,9 @@
+document.getElementById("sw-button").addEventListener("click",  function(){
+    var x = document.getElementById("more-info");
+    var y = document.getElementById("sw-button");
+    
+    x.classList.toggle("show-info");
+    y.classList.toggle("rot");
+
+        
+}); 
